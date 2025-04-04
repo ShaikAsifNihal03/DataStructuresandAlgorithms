d1278c264a627e2a4b0e5646ac0e01c39d01cd87
Shaik-Asif_Nihal <work.asifnihal@gmail.com>
2025-04-05 04:21:58 +0530

    Add files via upload

---
+++ Basics of Programming -II/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int a = 10;
+  int b = 5;
+//arithmetic operator 
+  int answer = a % b ;
+  cout << answer << endl;
+
+ //Assignment operator 
+  int age =18;
+  cout <<endl;
+
+ //relational operator 
+  cout << (a==b);
+  cout << (a!=b);
+  cout << (a<b);
+  cout << (a<b);
+  cout << (a>=b);
+  cout << (a<=b);
+  cout << endl;
+ 
+  //logical operator 
+  //logical and (&&)
+
+   char gender = 'M';
+   cout << ((age>18) && (gender == 'M'))<< endl; 
+  return 0;
+}
